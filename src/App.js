e0ffbe80210f725f2c6084cf761792c331441432
Niv Suroghon <nsuroghon@gmail.com>
2021-06-17 14:03:08 -0400

    readability

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ function App() {
   return (
       <div className="card max-w-sm rounded overflow-hidden shadow-lg">
         <img className="w-full" src="https://source.unsplash.com/random" alt=""></img>
+        
+        {/* photographer */}
         <div className="px-6 py-4">
           <div className="font-bold text-xl text-purple-500 mb-2">Photo by John Doe</div>
         </div>
+
+        {/* Sub headings */}
           <ul>
             <li>
               <strong>
