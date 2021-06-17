4578abf02a0df3c9a05231653f0edfd9e891a01b
Niv Suroghon <nsuroghon@gmail.com>
2021-06-17 13:59:52 -0400

    photographer name in card

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
       <div className="card max-w-sm rounded overflow-hidden shadow-lg">
         <img className="w-full" src="https://source.unsplash.com/random" alt=""></img>
-        
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl text-purple-500 mb-2">Photo by John Doe</div>
+        </div>
 
         
       </div>
