c701a9c79563141f09804c8780d304828be462c3
Niv Suroghon <nsuroghon@gmail.com>
2021-06-17 14:01:35 -0400

    subheader card

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
         <div className="px-6 py-4">
           <div className="font-bold text-xl text-purple-500 mb-2">Photo by John Doe</div>
         </div>
-
+          <ul>
+            <li>
+              <strong>
+                Views: 1000
+              </strong>
+            </li>
+          </ul>
         
       </div>
 
