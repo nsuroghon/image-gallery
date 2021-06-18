c19f25472f5763cce99fcd6795c201b4e6884ff3
Niv Suroghon <nsuroghon@gmail.com>
2021-06-18 11:24:52 -0400

    moving api photos from console to state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [images , setImage] = useState([]);
+  const [images , setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   // if left empty returns random photos
   const [term, setTerm] = useState("");
@@ -9,7 +9,10 @@ function App() {
   useEffect(() => {
     fetch(`https://pixabay.com/api/?key=22126125-95b100b3f165139e739090fac&q=${term}&image_type=photo&pretty=true`)
     .then(async res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      setImages(data.hits);
+      setIsLoading(false);
+    })
     .catch(err => console.log(err))
   }, []);
 
